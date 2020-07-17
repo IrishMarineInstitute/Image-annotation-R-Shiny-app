043b18e94c4748779b7bc9ee01c0519ef5552e1a
Mikel Aristegui <maristegui@Marine.ie>
2020-07-17 20:44:11 +0100

    stn250 instead of stn_250

--- Parent_folder/subfolder_with_the_app/app.R
+++ Parent_folder/subfolder_with_the_app/app.R
@@ -1760,7 +1760,13 @@ observeEvent({feat$counter}, {
                                      pattern = input$inSurveyID),
                                pattern = "counts.csv", value = T),
                           pattern = "SURVEYS_and_COUNTERS|Box|ERROR|SIC_", inv=T, value=T)
-        pairs.stn <- sub("*_(.?) *_.*", "_\\1", sub(paste0(".*", input$inSurveyID, "_ *(.*?) *_counts.*"), "\\1", pre.pairs))
+        
+        if (any(grepl("stn_", pre.pairs) == T)) {
+          pairs.stn <- sub("*_(.?) *_.*", "_\\1", sub(paste0(".*", input$inSurveyID, "_ *(.*?) *_counts.*"), "\\1", pre.pairs)) # station format: stn_250
+        } else {
+          pairs.stn <- sub("*_(.?) *.*", "\\", sub(paste0(".*", input$inSurveyID, "_ *(.*?) *_counts.*"), "\\1", pre.pairs)) # station format: stn250
+        }
+        
         pairs <- as.data.frame(cbind("file" = as.character(pre.pairs), "st" = as.character(pairs.stn)))
         pairs <- subset(pairs, st %in% unique(pairs$st[duplicated(pairs$st)])) # only if more than 1 counter has counted the station
         pairs[,1:2] <- lapply(pairs[,1:2], as.character)
